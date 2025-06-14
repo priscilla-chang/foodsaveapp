11ef2beac6b8ecf52404f317d2e8b36e65ee8d64
tzuchien <priscilla.chang31@gmail.com>
2025-06-14 11:27:15 +0800

    test1

--- firestore/firebase.js
+++ firestore/firebase.js
@@ -5,6 +5,7 @@ import { initializeApp } from 'firebase/app';
 // import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+//d
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCs56YYb2T8D_5QpAVf1mze2LVv7UYC62A',
